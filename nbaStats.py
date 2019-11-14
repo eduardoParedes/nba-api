b89426818b6d4fcb4e33a9fcb25e6688651bde39
eddywalls <eddieparedes12@gmail.com>
2019-11-14 08:48:21 -0500

    get teams

--- nbaStats.py
+++ nbaStats.py
@@ -1,1 +1,16 @@
-# first comment
+import requests
+from nba_api.stats.static import teams
+
+nba_teams = teams.get_teams()
+
+celtics = [team for team in nba_teams if team['abbreviation'] == 'BOS'][0]
+celtics_id = celtics['id']
+
+from nba_api.stats.endpoints import leaguegamefinder
+
+gamefinder = leaguegamefinder.LeagueGameFinder(team_id_nullable=celtics_id)
+
+games = gamefinder.get_data_frames()[0]
+print(games.head())
+
+# games.groupby(games.SEASON_ID.str[-4:][['GAME_ID']].count().loc['2015':])
